74617595e26bf8e55c4782d57d02e63024962273
Nico <foussier_nicolas@yahoo.fr>
2019-05-13 22:43:53 +0200

    add cret reate_time_laps function

--- pi-timelapse.py
+++ pi-timelapse.py
@@ -7,8 +7,10 @@
 import time
 import os
 import configparser
+import glob
 from picamera import PiCamera
 import RPi.GPIO as GPIO
+import subprocess
 
 def config_cam(file_config,camera):
 
@@ -22,16 +24,15 @@ def config_cam(file_config,camera):
 
 #config directory
 
-def config_directory(value_GPIO):
+def config_directory(value_GPIO,curent_directory):
     
-    current_directory=os.getcwd()
-
     if value_GPIO:
         name=time.strftime('%d_%B_%Y_%H_%M_%S')
         directory = os.path.join(current_directory,"scene",name)
-        os.mkdir(directory)            
+        os.mkdir(directory)
+        print(directory)
     else : 
-        directory_scene = os.path.join(os.getcwd(),"scene")
+        directory_scene = os.path.join(curent_directory,"scene")
         os.chdir(directory_scene)
         list_dir=os.listdir()       
         if list_dir !=[]:
@@ -57,7 +58,7 @@ def config_directory(value_GPIO):
 
 def get_last_photo(directory):
     
-    list_dir=os.listdir(directory)
+    list_dir=glob.glob('{0}/image*.jpg'.format(directory))
     
     
     if list_dir==[]:
@@ -90,17 +91,37 @@ def get_last_photo(directory):
     return nb_photo
 
 
+def create_time_lapse():
+
+    directory = config_directory(False,current_directory)
+    mpeg_file=glob.glob('{0}/*.mp4'.format(directory))
+    print(mpeg_file)
+    if len(mpeg_file) != 0 :
+
+        for files in mpeg_file :
+
+            os.remove('{0}'.format(files))
+   
+    os.chdir(directory)    
+    command=['avconv','-i','image%04d.jpg','-r','10','out.mp4']
+    subprocess.run(command,shell=False)
 
     
 
 
-memory=input("est ce une nouvelle session oui=1 et non =0")
+
+
+
+memory=int(input("est ce une nouvelle session oui=1 et non =0"))
 
 if memory == 1:
     memory =True
 else :
     memory = False
 
+current_directory = os.getcwd()
+
+print(current_directory)
 
 camera=PiCamera()
 
@@ -113,10 +134,12 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(12, GPIO.IN)
 
 
-directory=config_directory(memory)
+directory=config_directory(memory,current_directory)
+print(directory)
 last_image = get_last_photo(directory)
 
 
 camera.start_preview()
 time.sleep(2)
 camera.capture(os.path.join(directory,'image{}.jpg'.format(last_image)))
+create_time_lapse()
